000dba74ad4373290435d8f5b7bd5a8a878aa00e
Anıl Yeşil <mehmet.yesil@ug.bilkent.edu.tr>
2024-07-08 09:22:50 +0300

    Update CIEntity.java
    first commit

--- src/main/java/gov/turkpatent/ci_data_api/model/CIEntity.java
+++ src/main/java/gov/turkpatent/ci_data_api/model/CIEntity.java
@@ -1,10 +1,13 @@
 package gov.turkpatent.ci_data_api.model;
 
+import jakarta.persistence.*;
 import lombok.Value;
 
+@Entity
 @Value
 public class CIEntity {
 
+    @Id
     Long id;
     String name;
     String CItype;
