3107cf75b73a809342bfdeb683501aa550adc95e
hayrullahalper <alperhayrullah@gmail.com>
2024-07-08 08:56:37 +0300

    refactor: change JpaRepository type

--- src/main/java/gov/turkpatent/ci_data_api/repository/CIRepository.java
+++ src/main/java/gov/turkpatent/ci_data_api/repository/CIRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class CIRepository extends JpaRepository<CIEntity, Long> {
+public interface CIRepository extends JpaRepository<CIEntity, Long> {
 }
