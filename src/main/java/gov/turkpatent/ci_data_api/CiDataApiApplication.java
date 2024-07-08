6817155e2fe34371d10432d58ae855f52b07c1f6
hayrullahalper <alperhayrullah@gmail.com>
2024-07-08 08:39:01 +0300

    init

---
+++ src/main/java/gov/turkpatent/ci_data_api/CiDataApiApplication.java
@@ -0,0 +1,13 @@
+package gov.turkpatent.ci_data_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiDataApiApplication.class, args);
+	}
+
+}
