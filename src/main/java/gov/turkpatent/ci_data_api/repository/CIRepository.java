51d94d6ebd357e6c0812d22ad3ec198cb13bd902
Anıl Yeşil <mehmet.yesil@ug.bilkent.edu.tr>
2024-07-08 09:17:57 +0300

    Update CIRepository.java

--- src/main/java/gov/turkpatent/ci_data_api/repository/CIRepository.java
+++ src/main/java/gov/turkpatent/ci_data_api/repository/CIRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CIRepository extends JpaRepository<CIEntity, Long> {
+
+    CIEntity findByNameContaining(String name);
+
 }
